d5fbbe2e55dc0332334a7e74f8aa4255f73af9b2
Luke Schimmel <lbs.lic@me.com>
2021-04-27 19:19:24 +0100

    up to 100 and down to 0 working

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+#############################
+###Shades Dictionary Setup###
+#############################
 
-#Shades Dictionary Setup
 shades = {
 	'Test Shade':{
 		'ADC Pin': 0,
@@ -9,4 +11,29 @@ shades = {
 	}
 }
 
-#Motor Controller Settings
+
+###############################
+###Motor Controller Settings###
+###############################
+
+#move up settings
+up_pin_1 = True
+up_pin_2 = False
+
+#move down settings
+down_pin_1 = False
+down_pin_2 = True
+
+#stop settings
+stop_pin_1 = False
+stop_pin_2 = False
+
+
+######################
+###General Settings###
+######################
+
+verbose = True
+sensor_poll_delay = 0.5 #seconds
+motor_timeout = 10 #seconds
+raise_exeption_on_movement_error = False
--- move_shade.py
+++ move_shade.py
@@ -4,6 +4,20 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
 
+####################################
+####Utility Function Definitions####
+####################################
+
+def vprint(string):
+	if config.verbose:
+		print(string)
+
+def check_time(start_time):
+	current_time = time.time()
+	time_elapsed = current_time - start_time
+	return int(time_elapsed)
+
+
 #############################################
 ####Command Line Argument / Input Parsing####
 #############################################
@@ -37,6 +51,14 @@ elif not (0 <= shade_position and shade_position <= 100):
 shade = config.shades[shade_name]
 
 
+#prints key attributes if verbose:
+if config.verbose:
+	print('Request Type:', request)
+	print('Shade Name:', shade_name)
+	print('TargetPosition', shade_position)
+	print('Shade Object from Config', shade)
+
+
 #############################################
 #### RPI GPIO Setup / MCP3008 Chip Setup ####
 #############################################
@@ -62,11 +84,93 @@ motor_2 = digitalio.DigitalInOut(motor_2)
 motor_1.direction = digitalio.Direction.OUTPUT
 motor_2.direction = digitalio.Direction.OUTPUT
 
-print('Raw ADC Value: ', sensor.value)
-print('ADC Voltage: ' + str(sensor.voltage) + 'V')
 
-motor_1.value, motor_2.value = True, True
-time.sleep(5)
-motor_1.value, motor_2.value = False, False
+#############################################
+###### Motor / Shade Control Functions ######
+#############################################
 
-print("Done")
+def get_direction_pins(direction):
+	"""Gets the pin settings for upward/downward motion from config"""
+	direction = direction.lower()
+
+	if direction == 'up':
+		return config.up_pin_1, config.up_pin_2
+	elif direction == 'down':
+		return config.down_pin_1, config.down_pin_2
+	elif direction == 'stop':
+		return config.stop_pin_1, config.stop_pin_2
+	else:
+		raise NotImplementedError('Direction must be either UP or DOWN')
+
+
+def check_voltage(sensor_object, cutoff):
+	"""Checks the ADC voltage VS the cuttoff, returns True if voltage >= cutoff voltage"""
+	vprint(f'Raw ADC Value: {sensor.value}')
+	if sensor_object.value >= cutoff:
+		vprint(f"HIT ADC voltage cuttoff. [ADC = {sensor_object.value}, Cutoff = {cutoff}]")
+		return True
+	else:
+		return False
+
+
+def move_shade(direction, TargetPosition, pin_1_object, pin_2_object, sensor_object, cutoff):
+	#input sterilization
+	direction = direction.lower()
+	#temporary check to make sure that the given input is valid
+	if direction == 'up' and TargetPosition == 100:
+		pass
+	elif direction == 'down' and TargetPosition == 0:
+		pass
+	else:
+		print("ERROR: Setting shade to intermediate positions not yet implemented")
+		return False
+
+	#turns on the motor in the correct direction
+	vprint("Turning on Motor...")
+	pin_1_object.value, pin_2_object.value = get_direction_pins(direction)
+
+	#stop logic for setting shade to position 0 or 100
+	if TargetPosition == 0 or TargetPosition == 100:
+		
+		#checks the sense voltage, stops the motors once a cirtain threshold is met or timeout is hit
+		moving = True
+		error = False
+		start_time = time.time()
+
+		while moving:
+			time.sleep(config.sensor_poll_delay)
+
+			motor_stopped = check_voltage(sensor_object, cutoff)
+			time_elapsed = check_time(start_time)
+
+			if motor_stopped:
+				vprint(f'Motor Movement Complete. \nTime Elapsed: {time_elapsed} seconds')
+				moving = False
+			elif time_elapsed >= config.motor_timeout:
+				vprint(f'ERROR: Motor Timeout ({time_elapsed}) hit.')
+				error = True
+				moving = False
+
+
+	#stops the motors (cuts off power)
+	vprint("Turning off Motor...")
+	pin_1_object.value, pin_2_object.value = get_direction_pins('stop')
+
+	if error:
+		return False
+	else:
+		return True
+
+
+###################################
+###### Motor / Shade Control ######
+###################################
+
+success = move_shade(request, shade_position, motor_1, motor_2, sensor, shade['Cutoff'])
+
+if (not success) and config.raise_exeption_on_movement_error:
+	vprint('Encountered an error during motor movement.')
+	raise Exception('MOTOR MOVEMENT FAILURE')
+
+
+print("Done.")
